64d1ba1dbf80e1fb76931d1b538a6a7ad9a54ed6
IDarkgenesis <byidarkgenesis@gmail.com>
2020-12-04 15:40:33 +0100

    Nose que he cambiado

--- src/Slide.h
+++ src/Slide.h
@@ -23,10 +23,7 @@ public:
 	void setDirection(vec2 direction) override;
 
 	vec4 getBoundingBox() override;
-
-
 	vector<ivec2> occupiedTiles() override;
-	vec4		 getBoundingBox() override;	// vec4 -> xmin, xmax, ymin, ymax
 
 	// ESPERAR A AVANCES
 	bool trackPlayerVertical();
